54f125721a3a1f3307948bcb0a5d2f46405c9251
unknown <mechatoro@robocon01.nc-toyama.ac.jp>
2018-04-05 18:37:47 +0900

    LCD,USBUART削除

--- rotary_enc_hard.cydsn/Generated_Source/PSoC5/cyfitter_cfg.c
+++ rotary_enc_hard.cydsn/Generated_Source/PSoC5/cyfitter_cfg.c
@@ -167,11 +167,8 @@ CYPACKED typedef struct
 /* IOPINS0_1 Address: CYREG_PRT1_DM0 Size (bytes): 8 */
 #define BS_IOPINS0_1_VAL ((const uint8 CYFAR *)0x480003F8u)
 
-/* IOPINS0_2 Address: CYREG_PRT2_DM0 Size (bytes): 8 */
-#define BS_IOPINS0_2_VAL ((const uint8 CYFAR *)0x48000400u)
-
 /* IOPINS0_3 Address: CYREG_PRT3_DM0 Size (bytes): 8 */
-#define BS_IOPINS0_3_VAL ((const uint8 CYFAR *)0x48000408u)
+#define BS_IOPINS0_3_VAL ((const uint8 CYFAR *)0x48000400u)
 
 
 /*******************************************************************************
@@ -375,6 +372,7 @@ void cyfitter_cfg(void)
 		static const cfg_memset_t CYCODE cfg_memset_list[] = {
 			/* address, size */
 			{(void CYFAR *)(CYREG_PRT0_DR), 16u},
+			{(void CYFAR *)(CYREG_PRT2_DR), 16u},
 			{(void CYFAR *)(CYREG_PRT4_DR), 48u},
 			{(void CYFAR *)(CYREG_PRT12_DR), 16u},
 			{(void CYFAR *)(CYDEV_UCFG_B0_P0_U0_BASE), 4096u},
@@ -411,7 +409,6 @@ void cyfitter_cfg(void)
 	/* Perform second pass device configuration. These items must be configured in specific order after the regular configuration is done. */
 	CYCONFIGCPY((void CYFAR *)(CYREG_PRT15_DR), (const void CYFAR *)(BS_IOPINS0_8_VAL), 10u);
 	CYCONFIGCPY((void CYFAR *)(CYREG_PRT1_DM0), (const void CYFAR *)(BS_IOPINS0_1_VAL), 8u);
-	CYCONFIGCPY((void CYFAR *)(CYREG_PRT2_DM0), (const void CYFAR *)(BS_IOPINS0_2_VAL), 8u);
 	CYCONFIGCPY((void CYFAR *)(CYREG_PRT3_DM0), (const void CYFAR *)(BS_IOPINS0_3_VAL), 8u);
 	/* Switch Boost to the precision bandgap reference from its internal reference */
 	CY_SET_REG8((void CYXDATA *)CYREG_BOOST_CR2, (CY_GET_REG8((void CYXDATA *)CYREG_BOOST_CR2) | 0x08u));
